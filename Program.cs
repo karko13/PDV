f4dfed56d61418d33ace10852333cbc0253bd89e
karko13 <145532713+karko13@users.noreply.github.com>
2023-11-14 19:23:51 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDV
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Upisite cjenu bez PDV-a");
+            decimal x = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("Upisite cjenu sa PDV-om");
+            decimal y = Convert.ToDecimal(Console.ReadLine());
+            Console.WriteLine("PDV je " + (((y / x) -1 ) * 100) + " % ");
+            Console.ReadKey();
+        }
+    }
+}
